4000d48a3bb7c73d6b43f776f39fb7a899c056c7
Artem Oleksandrenko <artem2008.zp@gmail.com>
2025-06-10 02:47:15 +0300

    Add first server

--- index.js
+++ index.js
@@ -1,9 +1,33 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    console.log('req --> ', req);
-    console.log('res --> ', res);
-})
+    switch (req.url) {
+        case '/':
+            res.end('home page');
+            break;
+        case '/about':
+            res.end('about page');
+            break;
+        case '/blog':
+            res.end('blog page');
+            break;
+        case '/contacts':
+            res.end('contacts page');
+            break;
+        default:
+            res.end('404 - not found!')
+            break;
+    }
+    // console.log('req --> ', req);
+    // console.log('res --> ', res);
+    // console.log('req method --> ', req.method);
+    // console.log('req url --> ', req.url);
+    // res.end('<h1 style = "color: red">hello from server!!!</h1>');
+});
 
-// console.log(http);
-// console.log(server);
+const port = 3000;
+
+server.listen(port, () => {
+    const startDate = new Date().toLocaleString();
+    console.log(`server start at ${startDate} and listen port ${port}`);
+});
